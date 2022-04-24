1ed3eea12f73318e805aff32d7e05e19c499cad7
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2022-04-24 15:28:44 -0700

    Add files via upload

---
+++ cogs/lookup_cmds.py
@@ -0,0 +1,100 @@
+from discord.ext import commands
+import wikipedia
+from textblob import Word
+import nltk
+'''nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger') 
+nltk.download('brown')'''
+from youtube_search import YoutubeSearch
+
+
+class lookup(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		#Checking to make sure this cog is loaded upon initialization. 
+		print('Lookup cog loaded')
+
+
+	@commands.command()
+	async def wiki(self, ctx, *, search):
+		'''
+		Search, summarize, and link a Wikipedia page.
+	
+		Keyword Arguments:
+        self                   	-- This object.
+        ctx                 	-- Defined bot command-prefix.
+        search                  -- User-defined query.
+        
+        Return Value:
+        Summary and URL of queried Wikipedia page.
+		'''
+		#summary from the queried wiki page is captured.
+		summary = wikipedia.summary(search)
+		#the entirety of the queried wiki page is captured.
+		page = wikipedia.page(search)
+		#only taking the URL attribute as this will display a thumbnail on discord.
+		url = page.url
+		#formatted string to send the URL then a newline of the summary.
+		await ctx.send(f'{url}\n{summary}')
+	
+	
+	@commands.command()
+	async def define(self, ctx, word):
+		'''
+		Find the definition of a word using the textblob Word package.
+	
+		Keyword Arguments:
+        self                   	-- This object.
+        ctx                 	-- Defined bot command-prefix.
+        word                    -- User-defined word.
+        
+        Return Value:
+        Definition (string) of requested word formatted to be italicized on Discord.
+		'''
+		#create a Word instance of the word parameter
+		text = Word(word)
+		#finding the definition of the user-defined word
+		result = text.definitions
+		#creating a formatted string to show the user-defined word first, then open the italics discord command.
+		send = f'{word}: *'
+		#result is a list, so we need to add all the words in the list to the send variable
+		for _ in result:
+			send += _
+		#close the italics Discord command
+		send += '*'
+		await ctx.send(send)
+
+
+	@commands.command()
+	async def yt(self, ctx, *, search):
+		'''
+		Search and link a youtube video.
+
+		Keyword Arguments:
+        self                   	-- This object.
+        ctx                 	-- Defined bot command-prefix.
+        search                  -- User-defined query.
+        
+        Return Value:
+        Youtube URL (string) result of query. The queried URL is added to the end of the base youtube URL.
+		'''
+		#send the request for user-defined query. 
+		query = YoutubeSearch(search, max_results=1).to_dict()
+		#access the dict from the query variable
+		result = query[0]
+		#base youtube url
+		url = 'https://www.youtube.com'
+		#format the unique URL to the end of the base URL
+		url += result['url_suffix']
+		await ctx.send(url)
+
+
+def setup(bot):
+	bot.add_cog(lookup(bot))
---
+++ main.py
@@ -0,0 +1,43 @@
+from discord.ext import commands
+import os
+import logging
+import bot_token
+
+#define what will trigger bot commands
+#https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html
+bot = commands.Bot(command_prefix='!')
+
+
+#keeps a running event log while bot is on
+#https://discordpy.readthedocs.io/en/stable/logging.html
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
+#posts message to console once the bot has succesfully logged into Discord
+#https://discordpy.readthedocs.io/en/stable/quickstart.html
+@bot.event
+async def on_ready():
+	print(f'{bot.user} Initialized')
+
+
+@bot.command()
+async def load(ctx, extension):
+	bot.load_extension(f'cogs.{extension}')
+
+
+@bot.command()
+async def unload(ctx, extension):
+	bot.unload_extension(f'cogs.{extension}')
+
+
+for filename in os.listdir('./cogs'):
+	if filename.endswith('.py'):
+		bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+#bot's authentication token with Discord
+bot.run(bot_token.DISCORD_TOKEN)
