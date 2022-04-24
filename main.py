4102865ddf4463827c94750ddca66da3765ebc91
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2022-04-24 15:37:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -24,16 +24,6 @@ async def on_ready():
 	print(f'{bot.user} Initialized')
 
 
-@bot.command()
-async def load(ctx, extension):
-	bot.load_extension(f'cogs.{extension}')
-
-
-@bot.command()
-async def unload(ctx, extension):
-	bot.unload_extension(f'cogs.{extension}')
-
-
 for filename in os.listdir('./cogs'):
 	if filename.endswith('.py'):
 		bot.load_extension(f'cogs.{filename[:-3]}')
