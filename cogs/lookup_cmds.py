06338e40085ad0df6b8ba237842ea77ab7689f8e
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2022-04-25 01:40:31 -0700

    Update lookup_cmds.py
    added reddit scraper

--- cogs/lookup_cmds.py
+++ cogs/lookup_cmds.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from matplotlib.pyplot import hot
 import wikipedia
 from textblob import Word
 import nltk
@@ -9,6 +10,8 @@ nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger') 
 nltk.download('brown')
 from youtube_search import YoutubeSearch
+import praw
+import bot_tokens as bt
 
 
 class lookup(commands.Cog):
@@ -94,6 +97,64 @@ class lookup(commands.Cog):
 		#format the unique URL to the end of the base URL
 		url += result['url_suffix']
 		await ctx.send(url)
+	
+	@commands.command()
+	async def reddit(self, ctx, sub_reddit=None, hot_or_new=None):
+		'''
+		!reddit (sub-name, hot or new (hot by default), number of posts (3 by default))
+
+		Keyword Arguments:
+        self                   	-- This object.
+        ctx                 	-- Defined bot command-prefix.
+        subreddit               -- User-defined subreddit.
+		hot_or_new				-- Sort results by hot or new. Hot is set by default.
+        
+        Return Value:
+        Reddit URLs (strings) from user-defined subreddit. This can be customized to 
+		pull from either hot (by default) or by new. 
+		'''
+		#read-only instance authenticates with reddit
+		reddit_read_only = praw.Reddit(
+		client_id = bt.REDDIT_CLIENT_ID,          	#your client id
+        client_secret = bt.REDDIT_CLIENT_SECRET,  	#your client secret
+        user_agent = bt.REDDIT_USER_AGENT, 			#your user agent
+		check_for_async=False)
+		#pulls the user-defined subreddit
+		subreddit = reddit_read_only.subreddit(sub_reddit)
+
+		if sub_reddit == 'popular' or sub_reddit == 'all':
+			front_list = []
+			reddit_read_only.subreddit('all')
+			for post in subreddit.hot(limit=3):
+				front_list.append(post.permalink)
+			url = 'https://www.reddit.com'
+			url_list = []
+			for _ in front_list:
+				url_list.append(f'{url}{_}')
+			for _ in url_list:
+				await ctx.send(_)
+
+		elif hot_or_new == 'new'.lower():
+			new_list = []
+			for post in subreddit.new(limit=3):
+				new_list.append(post.permalink)
+			url = 'https://www.reddit.com'
+			url_list = []
+			for _ in new_list:
+				url_list.append(f'{url}{_}')
+			for _ in url_list:
+				await ctx.send(_)
+
+		elif hot_or_new == "hot".lower() or hot_or_new == None:
+			hot_list = []
+			for post in subreddit.hot(limit=3):
+				hot_list.append(post.permalink)
+			url = 'https://www.reddit.com'
+			url_list = []
+			for _ in hot_list:
+				url_list.append(f'{url}{_}')
+			for _ in url_list:
+				await ctx.send(_)
 
 
 def setup(bot):
