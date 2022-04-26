8ef74751394dd876359173a7d0f322d3a8fa1611
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2022-04-26 01:33:39 -0700

    added initialization post message

--- cogs/tracking.py
+++ cogs/tracking.py
@@ -5,6 +5,10 @@ class Tracking(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 		
+    @commands.Cog.listener()
+    async def on_ready(self):
+	    print('Tracking cog loaded') #Checking to make sure this cog is loaded upon initialization.
+		
 
 #communicates with extension loader/reloader in main
 def setup(bot):
