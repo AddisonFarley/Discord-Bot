5286266909eee8f561879e8d8d6591b97727f192
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2022-04-26 23:43:51 -0700

    returns whole numbers without decimal

--- cogs/math.py
+++ cogs/math.py
@@ -9,19 +9,24 @@ class math_calculator(commands.Cog):
 	    print('Math cog loaded') #Checking to make sure this cog is loaded upon initialization.
 
 	@commands.command()
-	async def math(round(eval(equation), 2)):
+	async def math(self, ctx, *, equation):
 		'''
 		!math (equation)
 
 		Keyword Arguments:
         self                   	-- This object.
-        ctx                 	  -- Defined bot command-prefix.
+        ctx                 	-- Defined bot command-prefix.
         equation               	-- User-defined equation.
         
         Return Value:
         Floating point number evauluated from equation input by user. 
 		'''
-		await ctx.send(eval(equation)) #takes in string of equation and evaluates it
+		result = round(eval(equation), 2) #rounds answer to two decimals
+
+		if result // 1 != 0: #checks if answer has a decimal value other than zero
+			await ctx.send(result) #sends float
+		else:
+			await ctx.send(int(result)) #sends int
 
 #communicates with extension loader/reloader in main
 def setup(bot):
