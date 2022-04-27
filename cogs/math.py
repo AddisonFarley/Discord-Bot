d15b58a68724552867720425bd0761ebd58484a6
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2022-04-26 23:53:51 -0700

    updated docstring/added exception

--- cogs/math.py
+++ cogs/math.py
@@ -16,18 +16,23 @@ class math_calculator(commands.Cog):
 		Keyword Arguments:
         self                   	-- This object.
         ctx                 	-- Defined bot command-prefix.
-        equation               	-- User-defined equation.
-        
+        equation               	-- User-defined equation. This equation must have all operators
+								   and cannot do algebra. Using multiplication in the form x()
+								   will return an error, as eval() sees that as calling a function,
+								   not multiplication.
         Return Value:
         Floating point number evauluated from equation input by user. 
 		'''
-		result = round(eval(equation), 2) #rounds answer to two decimals
 
-		if result // 1 != 0: #checks if answer has a decimal value other than zero
-			await ctx.send(result) #sends float
-		else:
-			await ctx.send(int(result)) #sends int
+		try:
+			if eval(equation) // 1 != 0: #checks if answer has a decimal value other than zero
+				await ctx.send(round(eval(equation), 2)) #sends float
+			else:
+				await ctx.send(int(eval(equation))) #sends int
+		except:
+			await ctx.send(f'Could not evaluate the equation: {equation}')
 
+			
 #communicates with extension loader/reloader in main
 def setup(bot):
 	bot.add_cog(math_calculator(bot))
